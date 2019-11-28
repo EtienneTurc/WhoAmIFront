ac56d77cb9f40090ba9a7ac1b4e3310e59cc9678
paul <pazoulai@gmail.com>
2019-11-28 17:05:27 +0100

    adds theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'mdi',
-  },
-});
+    icons: {
+        iconfont: 'mdi',
+    },
+    theme: {
+        themes: {
+            light: {
+                secondary: "#3c6382",
+            },
+            dark: {
+                primary: "#F8F8F8",
+            }
+        },
+        options: {
+            customProperties: true
+        }
+    }
+})
