7f7e3d20af91c192113b3bd9532ea5bb78b657d4
paul <pazoulai@gmail.com>
2019-12-07 20:58:25 +0100

    corrects colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,25 +1,24 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
-import colors from 'vuetify/lib/util/colors';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
     icons: {
-        iconfont: 'mdi',
+        iconfont: "mdi"
     },
     theme: {
         themes: {
             light: {
-                c_background: "#F8F8F8",
-                c_orange: "#FF9800",
-                c_secondary: "#3c6382",
-
+                secondary: "#3c6382"
             },
-
+            dark: {
+                primary: "#F8F8F8"
+            }
         },
         options: {
             customProperties: true
         }
     }
-})
+});
