324c853234c649a9de288aa2fefdbaf6c3978ba1
paul <pazoulai@gmail.com>
2019-12-10 16:50:46 +0100

    Adds connexion status indicator

---
+++ src/components/Status.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-chip label class="ma-2" :class="connected ? 'green' : 'red'" text-color="white">
+    {{name}}
+    <v-icon v-if="connected" right>mdi-check</v-icon>
+    <v-icon v-else right>mdi-close</v-icon>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+  data: () => ({
+    connected: true
+  }),
+  methods: {
+    connect: function() {
+      console.log("connected");
+      this.connected = true;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/StatusList.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title>Connections:</v-card-title>
+
+    <v-card-text>
+      <v-divider></v-divider>
+      <Status name="Google"></Status>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import Status from "@/components/Status.vue";
+
+export default {
+  data: () => ({}),
+  components: { Status }
+};
+</script>
+
+<style lang="scss" scoped>
+.status-main {
+  background-color: white;
+}
+</style>
