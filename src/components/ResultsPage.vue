5039b027fb1f45cb9d1eb845f213376cba22510a
paul <pazoulai@gmail.com>
2019-12-10 16:51:07 +0100

    merge tabs into one component

--- src/components/ResultsPage.vue
+++ src/components/ResultsPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <v-container fluid align-content-center class="main">
+      <div class="title">
+        <p class="display-3">Your results</p>
+        <v-divider></v-divider>
+      </div>
       <v-layout justify-center row>
-        <v-tabs background-color="secondary" dark :grow="true">
+        <v-tabs class="elevation-6" background-color="secondary" dark :grow="true">
           <v-tabs-slider color="orange"></v-tabs-slider>
           <v-tab :href="'#tab_basic_info'">Basic information</v-tab>
-          <v-tab :href="'#tab_calendar'">Calendar</v-tab>
           <v-tab :href="'#tab_analytics'">Analytics</v-tab>
 
           <v-tab-item :value="'tab_basic_info'">
@@ -14,24 +17,39 @@
                 <v-flex xs5>
                   <b>Last name</b>
                   <br />
-                  <span v-if="user_info.names">{{
+                  <span v-if="user_info.names">
+                    {{
                     user_info.names[0].familyName | prettyName
-                  }}</span>
+                    }}
+                  </span>
                 </v-flex>
                 <v-flex xs5>
                   <b>First name</b>
                   <br />
-                  <span v-if="user_info.names">{{
+                  <span v-if="user_info.names">
+                    {{
                     user_info.names[0].givenName | prettyName
-                  }}</span>
+                    }}
+                  </span>
                 </v-flex>
 
                 <v-flex xs5>
-                  <b>Addresse mail</b>
+                  <b>Mail address</b>
                   <br />
-                  <span v-if="user_info.emailAddresses">{{
+                  <span v-if="user_info.emailAddresses">
+                    {{
                     user_info.emailAddresses[0].value
-                  }}</span>
+                    }}
+                  </span>
+                </v-flex>
+                <v-flex xs5>
+                  <b>Calendar events found</b>
+                  <br />
+                  <span v-if="user_info.emailAddresses">
+                    {{
+                    this.events.length
+                    }}
+                  </span>
                 </v-flex>
               </v-card-text>
             </v-card>
@@ -68,7 +86,10 @@
 @import url("https://fonts.googleapis.com/css?family=Fugaz+One&display=swap");
 
 .main {
-  padding: 0;
+  padding: 4em;
+}
+.title {
+  margin-bottom: 2em;
 }
 </style>
 
