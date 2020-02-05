77f2fe37d4eae17aa52c436a8eb0a753f5d98ec9
Etienne Turc <etienne.turc@ensta-paristech.fr>
2020-02-05 13:02:15 +0100

    Get Drive and getMail

--- src/components/tabs/AnalyticsTab.vue
+++ src/components/tabs/AnalyticsTab.vue
@@ -1,33 +1,51 @@
 <template>
-  <v-card flat tile :loading="isLoading" class="card">
-    <v-card-text v-if="!isLoading">{{ result }}</v-card-text>
-    <v-card-text v-else>
-      <p>
-        We're computing your results, please hold on for a few more seconds!
-      </p>
-    </v-card-text>
-  </v-card>
+	<v-card flat tile :loading="isLoading" class="card">
+		<v-card-text v-if="!isLoading">{{ result }}</v-card-text>
+		<v-card-text v-else>
+			<p>We're computing your results, please hold on for a few more seconds!</p>
+		</v-card-text>
+	</v-card>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    result: {},
-    isLoading: true
-  }),
-  methods: {
-    getAnalyticsResults: function() {
-      this.isLoading = true;
-      this.$http.get("http://localhost:5000" + "/analytics").then(res => {
-        this.result = res.data.result;
-        this.isLoading = false;
-      });
-    }
-  },
-  created: function() {
-    this.getAnalyticsResults();
-  }
-};
+	data: () => ({
+		result: {},
+		isLoading: true
+	}),
+	methods: {
+		getAnalyticsResults: function() {
+			this.isLoading = true
+			this.$http
+				.get(process.env.VUE_APP_API_URL + "/analytics")
+				.then(res => {
+					this.result = res.data.result
+					this.isLoading = false
+				})
+		},
+		getMails() {
+			this.isLoading = true
+			this.$http
+				.get(process.env.VUE_APP_API_URL + "/google/gmail")
+				.then(res => {
+					this.result = res.data.result
+					this.isLoading = false
+				})
+		},
+		getDrive() {
+			this.isLoading = true
+			this.$http
+				.get(process.env.VUE_APP_API_URL + "/google/drive")
+				.then(res => {
+					this.result = res.data.result
+					this.isLoading = false
+				})
+		}
+	},
+	created: function() {
+		this.getMails()
+	}
+}
 </script>
 
 <style lang="scss" scoped></style>
