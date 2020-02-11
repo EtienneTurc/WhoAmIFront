c01efb7e2a389f8b24ebe4871fb34503e0d26702
Paul <pazoulai@gmail.com>
2020-02-11 18:56:50 +0100

    working with new api

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
         <v-col>
           <router-view class="nav-view elevation-6" />
         </v-col>
-        <v-col cols="6" class="d-none d-lg-block">
+        <!-- <v-col cols="6" class="d-none d-lg-block">
           <v-container class="twitter elevation-6 pa-0">
             <TwitterFeed src="https://twitter.com/vuejs" data-link-color="#E79723"></TwitterFeed>
           </v-container>
-        </v-col>
+        </v-col>-->
       </v-row>
     </v-content>
 
@@ -68,6 +68,6 @@ export default {
   height: 3em;
 }
 .app {
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='3c6382' fill-opacity='0.05' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  background-color: rgb(35, 39, 41);
 }
 </style>
--- src/components/tabs/BasicInfoTab.vue
+++ src/components/tabs/BasicInfoTab.vue
@@ -1,26 +1,38 @@
 <template>
-  <v-card flat tile :loading="isLoading" class="card">
-    <v-card-text>
+  <v-card flat tile :loading="!loaded" class="card">
+    <v-card-text v-if="loaded">
       <v-flex xs5>
         <b>Last name</b>
         <br />
-        <span v-if="user_info.names">{{ user_info.names[0].familyName | prettyName }}</span>
+        <span>{{ people.lastName | prettyName }}</span>
       </v-flex>
       <v-flex xs5>
         <b>First name</b>
         <br />
-        <span v-if="user_info.names">{{ user_info.names[0].givenName | prettyName }}</span>
+        <span>{{ people.firstName | prettyName }}</span>
       </v-flex>
 
       <v-flex xs5>
-        <b>Mail address</b>
-        <br />
-        <span v-if="user_info.emailAddresses">{{ user_info.emailAddresses[0].value }}</span>
+        <b>Adresses mail</b>
+        <template v-for="mail in people.mails">
+          <p :key="mail">{{ mail }}</p>
+        </template>
+      </v-flex>
+      <v-flex xs5>
+        <b>Adresses</b>
+        <template v-for="address in people.addresses">
+          <p :key="address">{{ address }}</p>
+        </template>
       </v-flex>
       <v-flex xs5>
         <b>Calendar events found</b>
         <br />
-        <span v-if="user_info.emailAddresses && !isLoading">{{ this.events.length }}</span>
+        <span>{{ this.events.number }}</span>
+      </v-flex>
+      <v-flex xs5>
+        <b>Date of birth</b>
+        <br />
+        <span>{{ this.people.birthDate }}</span>
       </v-flex>
     </v-card-text>
   </v-card>
@@ -32,56 +44,31 @@ import utils from "../../utils/utils";
 export default {
   data: () => ({
     events: {},
+    mails: {},
+    people: {},
     user_info: {},
-    isLoading: true
+    loaded: false
   }),
   methods: {
     getSimpleAnalytics: function() {
       this.$http
-        .get(process.env.VUE_APP_API_URL + "/analytics/simple", {
+        .get(process.env.VUE_APP_API_URL + "/google/", {
           headers: utils.getHeaders(this)
         })
         .then(res => {
-          console.log(res);
+          this.people = res.data.people;
+          this.events = res.data.calendar;
+          this.mails = res.data.gmail;
+          this.loaded = true;
+          console.log(this.people);
+          console.log(this.events);
         })
         .catch(err => {
           console.log(err);
-        });
-    },
-    getCalendar: function() {
-      this.isLoading = true;
-      this.$http
-        .get(process.env.VUE_APP_API_URL + "/google/calendar", {
-          headers: utils.getHeaders(this)
-        })
-        .then(res => {
-          this.events = res.data;
-          this.$router.replace({ query: {} });
-          this.isLoading = false;
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
-    getPeopleInfo() {
-      this.isLoading = true;
-      this.$http
-        .get(process.env.VUE_APP_API_URL + "/google/people", {
-          headers: utils.getHeaders(this)
-        })
-        .then(res => {
-          this.user_info = res.data.data;
-          this.isLoading = false;
-        })
-        .catch(err => {
-          return err;
         });
     }
   },
   created: function() {
-    console.log("hey");
-    this.getPeopleInfo();
-    // this.getCalendar();
     this.getSimpleAnalytics();
   }
 };
