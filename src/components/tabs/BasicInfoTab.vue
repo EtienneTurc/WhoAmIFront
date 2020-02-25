ade2af3e5f10caaf2dcb6f0a339395b5a4705022
Etienne Turc <etienne.turc@ensta-paristech.fr>
2020-02-25 15:36:23 +0100

    Distribution chart plot

--- src/components/tabs/BasicInfoTab.vue
+++ src/components/tabs/BasicInfoTab.vue
@@ -1,75 +1,81 @@
 <template>
-  <v-card flat tile :loading="!loaded" class="card">
-    <v-card-text v-if="loaded">
-      <v-flex xs5>
-        <b>Last name</b>
-        <br />
-        <span>{{ people.lastName | prettyName }}</span>
-      </v-flex>
-      <v-flex xs5>
-        <b>First name</b>
-        <br />
-        <span>{{ people.firstName | prettyName }}</span>
-      </v-flex>
+	<v-card flat tile :loading="!loaded" class="card">
+		<v-card-text v-if="loaded">
+			<v-flex xs5>
+				<b>Last name</b>
+				<br />
+				<span>{{ people.lastName | prettyName }}</span>
+			</v-flex>
+			<v-flex xs5>
+				<b>First name</b>
+				<br />
+				<span>{{ people.firstName | prettyName }}</span>
+			</v-flex>
 
-      <v-flex xs5>
-        <b>Adresses mail</b>
-        <template v-for="mail in people.mails">
-          <p :key="mail">{{ mail }}</p>
-        </template>
-      </v-flex>
-      <v-flex xs5>
-        <b>Adresses</b>
-        <template v-for="address in people.addresses">
-          <p :key="address">{{ address }}</p>
-        </template>
-      </v-flex>
-      <v-flex xs5>
-        <b>Calendar events found</b>
-        <br />
-        <span>{{ this.events.number }}</span>
-      </v-flex>
-      <v-flex xs5>
-        <b>Date of birth</b>
-        <br />
-        <span>{{ this.people.birthDate }}</span>
-      </v-flex>
-    </v-card-text>
-  </v-card>
+			<v-flex xs5>
+				<b>Adresses mail</b>
+				<template v-for="mail in people.mails">
+					<p :key="mail">{{ mail }}</p>
+				</template>
+			</v-flex>
+			<v-flex xs5>
+				<b>Adresses</b>
+				<template v-for="address in people.addresses">
+					<p :key="address">{{ address }}</p>
+				</template>
+			</v-flex>
+			<v-flex xs5>
+				<b>Calendar events found</b>
+				<br />
+				<span>{{ this.events.number }}</span>
+			</v-flex>
+			<v-flex xs5>
+				<b>Date of birth</b>
+				<br />
+				<span>{{ this.people.birthDate }}</span>
+			</v-flex>
+		</v-card-text>
+		<chart
+			:distribution=" this.mails.received.distribution"
+			:startDate=" this.mails.received.startDate"
+		></chart>
+	</v-card>
 </template>
 
 <script>
-import utils from "../../utils/utils";
+import utils from "../../utils/utils"
+import Chart from "../Chart"
 
 export default {
-  data: () => ({
-    events: {},
-    mails: {},
-    people: {},
-    user_info: {},
-    loaded: false
-  }),
-  methods: {
-    getSimpleAnalytics: function() {
-      this.$http
-        .get(process.env.VUE_APP_API_URL + "/google/", {
-          headers: utils.getHeaders(this)
-        })
-        .then(res => {
-          this.people = res.data.people;
-          this.events = res.data.calendar;
-          this.mails = res.data.gmail;
-          this.loaded = true;
-          console.log(this.people);
-          console.log(this.events);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  },
-  created: function() {
-    this.getSimpleAnalytics();
-  }
-};
+	data: () => ({
+		events: {},
+		mails: { received: {}, sent: {} },
+		people: {},
+		user_info: {},
+		loaded: false
+	}),
+	components: {
+		Chart
+	},
+	methods: {
+		getSimpleAnalytics: function() {
+			this.$http
+				.get(process.env.VUE_APP_API_URL + "/google/", {
+					headers: utils.getHeaders(this)
+				})
+				.then(res => {
+					this.people = res.data.people
+					this.events = res.data.calendar
+					this.mails = res.data.gmail
+					this.loaded = true
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}
+	},
+	created: function() {
+		this.getSimpleAnalytics()
+	}
+}
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,18 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-    state: {
-        tokenGoogle: null
-    },
-    mutations: {
-        setTokenGoogle(state, token) {
-            state.tokenGoogle = token;
-        },
-        unsetTokenGoogle(state) {
-            state.tokenGoogle = null;
-        }
-    },
-    getters: {
-        googleConnected: state => state.googleConnected
-    }
+	state: {
+		tokenGoogle: null
+	},
+	mutations: {
+		setTokenGoogle(state, token) {
+			state.tokenGoogle = token;
+		},
+		unsetTokenGoogle(state) {
+			state.tokenGoogle = null;
+		}
+	},
+	getters: {
+		googleConnected: state => state.googleConnected
+	}
 })
