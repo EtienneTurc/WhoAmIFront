a8ba9332dc31409f3129ca471bf8ac7467f5849c
Paul <pazoulai@gmail.com>
2020-03-03 12:38:18 +0100

    changes color scheme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,20 +5,20 @@ import colors from "vuetify/lib/util/colors";
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    icons: {
-        iconfont: "mdi"
+  icons: {
+    iconfont: "mdi"
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#2C3E50",
+        secondary: "#0184AF",
+        background: "#F5F5F5"
+      },
+      dark: {}
     },
-    theme: {
-        themes: {
-            light: {
-                secondary: "#3c6382"
-            },
-            dark: {
-                primary: "#F8F8F8"
-            }
-        },
-        options: {
-            customProperties: true
-        }
+    options: {
+      customProperties: true
     }
+  }
 });
