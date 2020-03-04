d0ff26feefb50180f91b2f8416918904bd14137c
Etienne Turc <etienne.turc@ensta-paristech.fr>
2020-03-04 11:11:55 +0100

    Map scroll on click + no more overlay + map centered

--- src/components/sections/MapSection.vue
+++ src/components/sections/MapSection.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="section map">
-    <Map
-      :height="'90%'"
-      :width="'90%'"
-      :places="['ENSTA Paristech', '5 rue Mizon, 75015 Paris', '2 boulevard du général Auguste Dubail, 90000 Belfort']"
-    ></Map>
-  </div>
+	<div class="section map">
+		<Map
+			:height="'90%'"
+			:width="'90%'"
+			:places="['ENSTA Paristech', '5 rue Mizon, 75015 Paris', '2 boulevard du général Auguste Dubail, 90000 Belfort']"
+		></Map>
+	</div>
 </template>
 
 <script>
-import Map from "@/components/Map";
+import Map from "@/components/utils/Map"
 export default {
-  components: {
-    Map
-  }
-};
+	components: {
+		Map
+	}
+}
 </script>
 
 <style lang="scss" scoped>
 .map {
-  padding: auto;
+	margin: auto;
+	vertical-align: middle;
 }
 </style>
-
