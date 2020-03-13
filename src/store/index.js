5038507614d975852b884655c7688b68b3dd723f
Paul <pazoulai@gmail.com>
2020-03-13 10:58:34 +0100

    improves mutations coherence

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import lodash from "lodash";
+let config = require("../config/config.json");
 
 Vue.use(Vuex);
 
-import googleHelpers from "./api";
+import { googleHelpers, facebookHelpers, fetchers } from "./api";
+import { cleaners } from "./helpers";
+import { getSessions } from "../utils/login";
 
 export const store = new Vuex.Store({
   state: {
     drawerOpen: false,
+    connexions: {
+      google: false,
+      facebook: false,
+      linkedin: false
+    },
     basic: {
       google: {
         attemptingConnection: false,
@@ -15,6 +24,10 @@ export const store = new Vuex.Store({
         error: false,
         peopleData: null,
         mailData: null
+      },
+      facebook: {
+        peopleData: null,
+        fetching: false
       }
     },
     analytics: {
@@ -26,31 +39,67 @@ export const store = new Vuex.Store({
     }
   },
   actions: {
-    async GET_GOOGLE_DATA(context) {
-      context.commit("SET_GOOGLE_FETCHING_STATE", true);
-      let promises = [
-        googleHelpers.analytics.getMailsAnalytics(context, this._vm),
-        googleHelpers.basic.getPeople(context, this._vm),
-        googleHelpers.basic.getMailsBasic(context, this._vm)
-      ];
-      await Promise.all(promises);
-      context.commit("SET_GOOGLE_FETCHING_STATE", false);
+    async SYNC_DATA(context) {
+      Object.keys(context.state.connexions).forEach(service => {
+        console.log(
+          service,
+          context.state.connexions[service],
+          context.getters[`${service}Data`]
+        );
+
+        if (
+          context.state.connexions[service] &&
+          !context.getters[`${service}Data`]
+        ) {
+          console.log("fetching data for service ", service);
+
+          // get data if service is connected and no data
+          fetchers.basic[service].forEach(apiCaller => {
+            apiCaller(context, this._vm);
+          });
+          fetchers.analytics[service].forEach(apiCaller => {
+            // console.log(apiCaller);
+            apiCaller(context, this._vm);
+          });
+          // console.log(service);
+        } else if (
+          !context.state.connexions[service] &&
+          Object(context.getters)[`${service}Data`]
+        ) {
+          // remove data when service is connected and there is data
+          console.log("removing data for service ", service);
+          cleaners[service](context);
+        }
+      });
+      return;
     },
-    REMOVE_DATA(context) {
-      context.commit("SET_GOOGLE_PEOPLE_DATA", null);
-      context.commit("SET_GOOGLE_ANALYTICS_DATA", null);
-    },
-    REMOVE_GOOGLE_DATA(context) {
-      context.commit("SET_GOOGLE_PEOPLE_DATA", null);
-      context.commit("SET_GOOGLE_ANALYTICS_DATA", null);
+    async GET_CONNEXIONS_STATUS(context) {
+      // updates front end connexions based on backend
+
+      let backendSessions = await getSessions(this._vm);
+      let frontendSessions = context.state.connexions;
+
+      Object.keys(frontendSessions).forEach(session => {
+        context.commit("SET_STATUS_CONNEXION", {
+          service: session,
+          connexion: backendSessions.includes(session) // true or false
+        });
+      });
+      return;
     }
   },
   mutations: {
+    SET_STATUS_CONNEXION(state, params) {
+      Vue.set(state.connexions, params.service, params.connexion);
+    },
     SET_DRAWER_STATE(state, isOpen) {
       state.drawerOpen = isOpen;
     },
     SET_GOOGLE_PEOPLE_DATA(state, payload) {
       Vue.set(state.basic.google, "peopleData", payload);
+    },
+    SET_FACEBOOK_PEOPLE_DATA(state, payload) {
+      Vue.set(state.basic.facebook, "peopleData", payload);
     },
     SET_GOOGLE_MAIL_DATA(state, payload) {
       Vue.set(state.basic.google, "mailData", payload);
@@ -60,6 +109,9 @@ export const store = new Vuex.Store({
     },
     SET_GOOGLE_FETCHING_STATE(state, payload) {
       state.basic.google.fetching = payload;
+    },
+    SET_FACEBOOK_FETCHING_STATE(state, payload) {
+      state.basic.facebook.fetching = payload;
     },
     SET_GOOGLE_ANALYTICS_DATA(state, payload) {
       Vue.set(state.analytics.google, "data", payload);
@@ -72,9 +124,23 @@ export const store = new Vuex.Store({
     }
   },
   getters: {
-    googleConnected: state => {
-      let res = state.basic.google.peopleData != null;
-      return res;
+    googleData: state => {
+      let hasBasicData =
+        !_.isEmpty(state.basic.google.peopleData) ||
+        !_.isEmpty(state.basic.google.mailData);
+      let hasAnalyticsData = !_.isEmpty(state.analytics.google.data);
+      return (
+        hasAnalyticsData ||
+        hasBasicData ||
+        state.basic.google.fetching ||
+        state.analytics.google.fetching
+      );
+    },
+    facebookData: state => {
+      console.log(state.basic.facebook.peopleData);
+
+      let hasBasicData = !_.isEmpty(state.basic.facebook.peopleData);
+      return hasBasicData || state.basic.facebook.fetching;
     }
   }
 });
--- src/utils/login.js
+++ src/utils/login.js
@@ -16,8 +16,6 @@ let getConsentUrl = function(conf, state) {
 };
 
 let login = function(nextElements) {
-  console.log("now logging to ", nextElements);
-
   if (nextElements.length) {
     let el = nextElements.pop().toLowerCase();
 
@@ -32,14 +30,10 @@ let login = function(nextElements) {
 let getSessions = async function(vm) {
   let res = await vm.$http.get(process.env.VUE_APP_API_URL + "/login/loggedTo");
   let sessions = res.data;
-  console.log("logged to ", sessions);
-
   return sessions;
 };
 
 let logOut = async function(services, vm) {
-  console.log("logging out from", services);
-
   let sessions = await vm.$http.post(
     process.env.VUE_APP_API_URL + "/login/logout",
     {
