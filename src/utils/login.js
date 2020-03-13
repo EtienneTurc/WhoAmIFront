34f0c75a46ac62b2e34589d65e1063ef170ef1ed
Paul <pazoulai@gmail.com>
2020-03-13 10:57:04 +0100

    adds getSessions and logOut methods
    Co-authored-by: EtienneTurc <34250388+EtienneTurc@users.noreply.github.com>

--- src/utils/login.js
+++ src/utils/login.js
@@ -1,31 +1,51 @@
-import config from "../config/config.json"
+import config from "../config/config.json";
+import Vue from "vue";
 
-let getConsentUrl = function (conf, state) {
-	let url = conf.url
-	let queries = conf.params
-	queries.scope = queries.scope.join(conf.scope_separator)
-	let i = 0
-	for (let q in queries) {
-		url += i ? "&" : ""
-		url +=
-			encodeURIComponent(q) + "=" + encodeURIComponent(queries[q])
-		i++
-	}
-	url += "&state=" + encodeURIComponent(state.join(','))
-	return url
-}
+let getConsentUrl = function(conf, state) {
+  let url = conf.url;
+  let queries = conf.params;
+  queries.scope = queries.scope.join(conf.scope_separator);
+  let i = 0;
+  for (let q in queries) {
+    url += i ? "&" : "";
+    url += encodeURIComponent(q) + "=" + encodeURIComponent(queries[q]);
+    i++;
+  }
+  url += "&state=" + encodeURIComponent(state.join(","));
+  return url;
+};
 
-let login = function (nextElements) {
-	if (nextElements.length) {
-		let el = nextElements.pop().toLowerCase()
+let login = function(nextElements) {
+  console.log("now logging to ", nextElements);
 
-		if (!config[el]) {
-			login(nextElements)
-		} else {
-			window.location.replace(getConsentUrl(config[el], nextElements))
-		}
-	}
+  if (nextElements.length) {
+    let el = nextElements.pop().toLowerCase();
 
-}
+    if (!config[el]) {
+      login(nextElements);
+    } else {
+      window.location.replace(getConsentUrl(config[el], nextElements));
+    }
+  }
+};
 
-export { login };
+let getSessions = async function(vm) {
+  let res = await vm.$http.get(process.env.VUE_APP_API_URL + "/login/loggedTo");
+  let sessions = res.data;
+  console.log("logged to ", sessions);
+
+  return sessions;
+};
+
+let logOut = async function(services, vm) {
+  console.log("logging out from", services);
+
+  let sessions = await vm.$http.post(
+    process.env.VUE_APP_API_URL + "/login/logout",
+    {
+      services: services
+    }
+  );
+};
+
+export { login, getSessions, logOut };
